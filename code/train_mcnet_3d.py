d9fd5145212df2701697fbfaae625854ca3b5de4
Pierre Rougé <rouge@eq12-om-771.creatis.insa-lyon.fr>
2023-08-22 17:37:09 +0200

    try to fix type bug

--- code/train_mcnet_3d.py
+++ code/train_mcnet_3d.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
         for i_batch, sampled_batch in enumerate(trainloader):
             
             volume_batch, label_batch = sampled_batch['image'], sampled_batch['label']
-            volume_batch, label_batch = volume_batch.cuda(), label_batch.cuda()
+            volume_batch, label_batch = volume_batch.cuda(), label_batch.cuda().float()
 
             model.train()
             outputs = model(volume_batch)
