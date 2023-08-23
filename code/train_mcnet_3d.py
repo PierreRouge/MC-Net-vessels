09fc0d8e41dd0cf0d6ea2a6edf8209ac222d6b9f
Pierre Rougé <rouge@eq12-om-771.creatis.insa-lyon.fr>
2023-08-23 11:11:05 +0200

    modify test during training

--- code/train_mcnet_3d.py
+++ code/train_mcnet_3d.py
@@ -171,7 +171,7 @@ if __name__ == "__main__":
             writer.add_scalar('Co_loss/consistency_loss', loss_consist, iter_num)
             writer.add_scalar('Co_loss/consist_weight', consistency_weight, iter_num)
 
-            if iter_num >= 800 and iter_num % 200 == 0:
+            if iter_num >= 8000 and iter_num % 8000 == 0:
                 ins_width = 2
                 B,C,H,W,D = y_ori[0].size()
                 snapshot_img = torch.zeros(size = (D, 3, (num_outputs + 2) *H + (num_outputs + 2) * ins_width, W + ins_width), dtype = torch.float32)
@@ -200,7 +200,7 @@ if __name__ == "__main__":
                     snapshot_img[:, 2, begin*H+ begin*ins_width:end*H+ begin*ins_width,:W] = y_ori[idx][labeled_bs:][0,1].permute(2,0,1)
                 writer.add_images('Epoch_%d_Iter_%d_unlabel'% (epoch_num, iter_num), snapshot_img)
                 
-            if iter_num >= 800 and iter_num % 200 == 0:
+            if iter_num >= 8000 and iter_num % 8000 == 0:
                 model.eval()
                 if args.dataset_name =="LA":
                     dice_sample = test_patch.var_all_case(model, num_classes=num_classes, patch_size=patch_size, stride_xy=18, stride_z=4, dataset_name = 'LA')
