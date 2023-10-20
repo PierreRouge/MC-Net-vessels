fd00498d9f6b36f230cbc58b8004c131afe9cb7b
Pierre Rougé <rouge@eq2-cf-626.creatis.insa-lyon.fr>
2023-10-20 10:30:05 +0200

    add flag patch_size

--- code/test_3d.py
+++ code/test_3d.py
@@ -11,34 +11,39 @@ parser.add_argument('--root_path', type=str, default='./', help='Name of Experim
 parser.add_argument('--exp', type=str,  default='MCNet', help='exp_name')
 parser.add_argument('--model', type=str,  default='mcnet3d_v1', help='model_name')
 parser.add_argument('--gpu', type=str,  default='0', help='GPU to use')
+parser.add_argument('--patch_size', nargs='+', type=int, default=[128, 128, 128], help='Patch _size')
 parser.add_argument('--detail', type=int,  default=1, help='print metrics for every samples?')
 parser.add_argument('--labelnum', type=int, default=16, help='labeled data')
 parser.add_argument('--nms', type=int, default=0, help='apply NMS post-procssing?')
 
 FLAGS = parser.parse_args()
 os.environ['CUDA_VISIBLE_DEVICES'] = FLAGS.gpu
-snapshot_path = FLAGS.root_path + "model/{}_{}_{}_labeled/{}".format(FLAGS.dataset_name, FLAGS.exp, FLAGS.labelnum, FLAGS.model)
-test_save_path = FLAGS.root_path + "model/{}_{}_{}_labeled/{}_predictions/".format(FLAGS.dataset_name, FLAGS.exp, FLAGS.labelnum, FLAGS.model)
+snapshot_path = "../model/{}_{}_{}_labeled/{}".format(FLAGS.dataset_name, FLAGS.exp, FLAGS.labelnum, FLAGS.model)
+test_save_path = "../model/{}_{}_{}_labeled/{}_predictions/".format(FLAGS.dataset_name, FLAGS.exp, FLAGS.labelnum, FLAGS.model)
 
 num_classes = 2
 if FLAGS.dataset_name == "LA":
-    patch_size = (128, 128, 96)
-    FLAGS.root_path = FLAGS.root_path + 'data/LA'
-    with open(FLAGS.root_path + '/test.list', 'r') as f:
+    patch_size = FLAGS.patch_size
+    with open(FLAGS.root_path + '/../test.list', 'r') as f:
         image_list = f.readlines()
     image_list = [FLAGS.root_path + "/2018LA_Seg_Training Set/" + item.replace('\n', '') + "/mra_norm.h5" for item in image_list]
     
 elif FLAGS.dataset_name == "IXI":
-    patch_size = (128, 128, 96)
-    FLAGS.root_path = FLAGS.root_path + '../data'
-    with open(FLAGS.root_path + '/test.list', 'r') as f:
+    patch_size = FLAGS.patch_size
+    with open(FLAGS.root_path + '/../test.list', 'r') as f:
         image_list = f.readlines()
     image_list = [FLAGS.root_path + "/IXI_Bullitt_training_set/" + item.replace('\n', '') + "/mra_norm.h5" for item in image_list]
 
+elif FLAGS.dataset_name == "Bullitt":
+    patch_size = FLAGS.patch_size
+    with open(FLAGS.root_path + '/../test.list', 'r') as f:
+        image_list = f.readlines()
+    image_list = [FLAGS.root_path + "/" + item.replace('\n', '') + "/mra_norm.h5" for item in image_list]
+    
 elif FLAGS.dataset_name == "Pancreas_CT":
-    patch_size = (96, 96, 96)
+    patch_size = FLAGS.patch_size
     FLAGS.root_path = FLAGS.root_path + 'data/Pancreas'
-    with open(FLAGS.root_path + '/test.list', 'r') as f:
+    with open(FLAGS.root_path + '/../test.list', 'r') as f:
         image_list = f.readlines()
     image_list = [FLAGS.root_path + "/Pancreas_h5/" + item.replace('\n', '') + "_norm.h5" for item in image_list]
 
@@ -56,17 +61,22 @@ def test_calculate_metric():
 
     if FLAGS.dataset_name == "LA":
         avg_metric = test_all_case(FLAGS.model, 1, net, image_list, num_classes=num_classes,
-                        patch_size=(128, 128, 96), stride_xy=18, stride_z=4,
+                        patch_size=FLAGS.patch_size, stride_xy=18, stride_z=4,
                         save_result=True, test_save_path=test_save_path,
                         metric_detail=FLAGS.detail, nms=FLAGS.nms)
     elif FLAGS.dataset_name == "Pancreas_CT":
         avg_metric = test_all_case(FLAGS.model, 1, net, image_list, num_classes=num_classes,
-                        patch_size=(96, 96, 96), stride_xy=16, stride_z=16,
+                        patch_size=FLAGS.patch_size, stride_xy=16, stride_z=16,
+                        save_result=True, test_save_path=test_save_path,
+                        metric_detail=FLAGS.detail, nms=FLAGS.nms)
+    elif FLAGS.dataset_name == "Bullitt":
+        avg_metric = test_all_case(FLAGS.model, 1, net, image_list, num_classes=num_classes,
+                        patch_size=FLAGS.patch_size, stride_xy=16, stride_z=16,
                         save_result=True, test_save_path=test_save_path,
                         metric_detail=FLAGS.detail, nms=FLAGS.nms)
     elif FLAGS.dataset_name == "IXI":
         avg_metric = test_all_case(FLAGS.model, 1, net, image_list, num_classes=num_classes,
-                        patch_size=(128, 128, 96), stride_xy=18, stride_z=4,
+                        patch_size=FLAGS.patch_size, stride_xy=18, stride_z=4,
                         save_result=True, test_save_path=test_save_path,
                         metric_detail=FLAGS.detail, nms=FLAGS.nms)
 
