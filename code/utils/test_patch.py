5f4a513cfe7cc17a145d2903e23dcf21dcf870c3
pierre.rouge <ubq47px@jean-zay1.idris.fr>
2023-11-19 21:08:34 +0100

    debug LA

--- code/utils/test_patch.py
+++ code/utils/test_patch.py
@@ -18,7 +18,7 @@ def var_all_case(model, num_classes, patch_size=(112, 112, 80), stride_xy=18, st
     if dataset_name == "LA":
         with open('../data/LA/test.list', 'r') as f:
             image_list = f.readlines()
-        image_list = ["../data/LA/2018LA_Seg_Training Set/" + item.replace('\n', '') + "/data.h5" for item in image_list]
+        image_list = ["../data/LA/2018LA_Seg_Training Set/Patients/" + item.replace('\n', '') + "/data.h5" for item in image_list]
     elif dataset_name == "Pancreas_CT":
         with open('./data/Pancreas/test.list', 'r') as f:
             image_list = f.readlines()
